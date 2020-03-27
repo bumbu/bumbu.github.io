937eab6c16d675fb9be3105f9ab2b86560fdf232
Alex Bumbu <bmbalex@gmail.com>
2020-03-26 21:21:49 -0700

    [Husky] Add north roof wall

--- _source/scripts/husky-rock/data.js
+++ _source/scripts/husky-rock/data.js
@@ -1112,8 +1112,258 @@ const southTowerWall = {
   },
 }
 
+const northRoof = {
+  key: 'north-roof',
+  name: 'North Roof',
+  image: 'north-roof.png',
+  width: 965,
+  height: 850,
+  rocks: {
+    edge1: {
+      top: 24,
+      left: 451,
+      width: 73,
+      height: 25,
+      type: 'edge',
+    },
+    edge2: {
+      top: 16,
+      left: 276,
+      width: 65,
+      height: 27,
+      type: 'edge',
+    },
+    edge3: {
+      top: 8,
+      left: 110,
+      width: 85,
+      height: 24,
+      type: 'edge',
+    },
+    rock0: {
+      top: 425,
+      left: 739,
+      width: 41,
+      height: 38,
+    },
+    rock1: {
+      top: 252,
+      left: 679,
+      width: 26,
+      height: 18,
+    },
+    rock2: {
+      top: 209,
+      left: 638,
+      width: 34,
+      height: 23,
+    },
+    rock3: {
+      top: 187,
+      left: 559,
+      width: 44,
+      height: 26,
+    },
+    rock4: {
+      top: 122,
+      left: 490,
+      width: 24,
+      height: 30,
+    },
+    rock5: {
+      top: 433,
+      left: 453,
+      width: 61,
+      height: 26,
+    },
+    rock6: {
+      top: 252,
+      left: 364,
+      width: 22,
+      height: 26,
+    },
+    rock7: {
+      top: 95,
+      left: 290,
+      width: 41,
+      height: 31,
+    },
+    rock8: {
+      top: 416,
+      left: 195,
+      width: 51,
+      height: 86,
+    },
+    rock9: {
+      top: 240,
+      left: 87,
+      width: 19,
+      height: 28,
+    },
+    rock10: {
+      top: 160,
+      left: 79,
+      width: 33,
+      height: 37,
+    },
+    rock11: {
+      top: 555,
+      left: 268,
+      width: 27,
+      height: 54,
+    },
+    rock12: {
+      top: 457,
+      left: 354,
+      width: 28,
+      height: 18,
+    },
+    rock13: {
+      top: 205,
+      left: 165,
+      width: 28,
+      height: 27,
+    },
+  },
+  routes: {
+    route1: {
+      name: null,
+      description: null,
+      type: ROUTE_TYPES.ARF,
+      grade: ROUTE_GRADES.MODERATE_HARD,
+      risk: ROUTE_RISKS.LOW,
+      sequence: [
+        {
+          key: 'rock0',
+        },
+        {
+          key: 'rock1',
+        },
+        {
+          key: 'rock2',
+        },
+        {
+          key: 'rock3',
+        },
+        {
+          key: 'rock4',
+        },
+        {
+          key: 'edge1',
+        },
+      ],
+    },
+    route2: {
+      name: '3-rock classic',
+      description: null,
+      type: ROUTE_TYPES.noF,
+      grade: ROUTE_GRADES.HARD,
+      risk: ROUTE_RISKS.LOW,
+      sequence: [
+        {
+          key: 'rock5',
+        },
+        {
+          key: 'rock6',
+        },
+        {
+          key: 'rock7',
+        },
+        {
+          key: 'edge2',
+        },
+      ],
+    },
+    route3: {
+      name: '3-rock classic (variation)',
+      description: null,
+      type: ROUTE_TYPES.ARF,
+      grade: ROUTE_GRADES.MODERATE_HARD,
+      risk: ROUTE_RISKS.LOW,
+      sequence: [
+        {
+          key: 'rock5',
+        },
+        {
+          key: 'rock6',
+        },
+        {
+          key: 'rock7',
+        },
+        {
+          key: 'edge2',
+        },
+      ],
+    },
+    route4: {
+      name: '3-rock classic (variation)',
+      description: 'Layback crack to tooth',
+      type: ROUTE_TYPES.noF,
+      grade: ROUTE_GRADES.HARD,
+      risk: ROUTE_RISKS.LOW,
+      sequence: [
+        {
+          key: 'rock5',
+        },
+        {
+          key: 'rock6',
+        },
+        {
+          key: 'rock7',
+        },
+        {
+          key: 'edge2',
+        },
+      ],
+    },
+    route5: {
+      name: 'Monkey Trap',
+      description: null,
+      type: ROUTE_TYPES.noF,
+      grade: ROUTE_GRADES.VERY_HARD,
+      risk: ROUTE_RISKS.LOW,
+      sequence: [
+        {
+          key: 'rock8',
+        },
+        {
+          key: 'rock9',
+        },
+        {
+          key: 'rock10',
+        },
+        {
+          key: 'edge3',
+        },
+      ],
+    },
+    route6: {
+      name: 'Tuff Enuf!',
+      description: 'Sit start',
+      type: ROUTE_TYPES.ARF,
+      grade: ROUTE_GRADES.EXTREMELY_HARD,
+      risk: ROUTE_RISKS.LOW,
+      sequence: [
+        {
+          key: 'rock11',
+        },
+        {
+          key: 'rock12',
+        },
+        {
+          key: 'rock13',
+        },
+        {
+          key: 'edge3',
+        },
+      ],
+    },
+  },
+}
+
 const WALLS = {
-  ['south-tower']: southTowerWall,
+  [southTowerWall.key]: southTowerWall,
+  [northRoof.key]: northRoof,
 }
 
 module.exports = {
